df374b2f5bfacc35d658547718bfd7d10eaaeee3
matt9388 <72760841+matt9388@users.noreply.github.com>
2023-11-15 12:43:47 -0600

    uploading TRS Exercise One Solution

---
+++ TRS_E1_CountVariations/Flavors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRS_E1_CountVariations
+{
+	internal class Flavors
+	{
+		/// <summary>
+		/// First flavor for the day.
+		/// </summary>
+		public string? FlavorOne { get; set; }
+		/// <summary>
+		/// Second flavor for the day.
+		/// </summary>
+		public string? FlavorTwo { get; set; }
+		/// <summary>
+		/// Third flavor for the day.
+		/// </summary>
+		public string? FlavorThree { get; set; }
+	}
+}
---
+++ TRS_E1_CountVariations/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace TRS_E1_CountVariations
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string filePath = @"flavors.json"; //Json file in root output to bin.
+			List<Flavors>? flavors = JsonConvert.DeserializeObject<List<Flavors>>(File.ReadAllText(filePath)); //Read Json file into list of Flavors object.
+
+			//Using Linq to group all flavors together and get the count of each combination.
+			var result = flavors.GroupBy(x => new { x.FlavorOne, x.FlavorTwo, x.FlavorThree }).Select(x => new { f1 = x.Key.FlavorOne, f2 = x.Key.FlavorTwo, f3 = x.Key.FlavorThree, total = x.Count() });
+
+			Console.WriteLine("{0,-50} {1}", " Combination", " Times Eaten");
+			Console.WriteLine(new string('-', 65));
+			foreach (var item in result)
+			{
+				Console.WriteLine("{0,-50} {1}", (" " + item.f1 + ", " + item.f2 + ", " + item.f3), "| " + item.total);
+			}
+			Console.Read();
+		}
+	}
+}
